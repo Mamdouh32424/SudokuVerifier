1f7378b8a4fb3f226ec80e0b7d990f5c13d00cbe
Mamdouh_Nafee <mamdouhdido5@gmail.com>
2025-11-28 02:27:33 +0200

    Mode0 done

--- src/Mode0.java
+++ src/Mode0.java
@@ -1,5 +1,77 @@
 public class Mode0 {
-    // mara etnen darabo b3d
-    // b3d et3awar 
 
+    private int [][] grid;
+
+    private Row[] rows = new Row[9];
+    private Column[] columns = new Column[9];
+    private Box[] boxes = new Box[9];
+
+    public Mode0(int[][] grid) {
+        this.grid = grid;
+
+        for(int i = 0; i < 9; i++){
+            rows[i] = new Row(grid,i);
+            columns[i] = new Column(grid,i);
+            boxes[i] = new Box(grid,i);
+        }
+
+    }
+
+    public void run() {
+        boolean valid = true;
+        valid &= scanRows();
+        valid &= scanColumns();
+        valid &= scanBoxes();
+
+        if(valid){
+            System.out.println("Valid");
+        }
+        else{
+            System.out.println("Invalid");
+            printRowErrors();
+            System.out.println("------------------------------------------");
+            printColumnErrors();
+            System.out.println("------------------------------------------");
+            printBoxErrors();
+        }
+    }
+
+
+    public boolean scanRows(){
+        boolean valid = true;
+        for(int i = 0;i<9;i++) {
+            valid &= rows[i].scan();
+        }
+        return valid;
+    }
+    public boolean scanColumns(){
+        boolean valid = true;
+        for(int i = 0;i<9;i++) {
+            valid &= columns[i].scan();
+        }
+        return valid;
+    }
+    public boolean scanBoxes(){
+        boolean valid = true;
+        for(int i = 0;i<9;i++) {
+            valid &= boxes[i].scan();
+        }
+        return valid;
+    }
+
+    public void printRowErrors(){
+        for(int i = 0;i<9;i++) {
+            rows[i].printError();
+        }
+    }
+    public void printColumnErrors(){
+        for(int i = 0;i<9;i++) {
+            columns[i].printError();
+        }
+    }
+    public void printBoxErrors(){
+        for(int i = 0;i<9;i++) {
+            boxes[i].printError();
+        }
+    }
 }
