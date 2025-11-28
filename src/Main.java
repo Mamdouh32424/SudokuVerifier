ff5aad35b2a479620e081e9d7d3db2bc6f275504
Mamdouh_Nafee <mamdouhdido5@gmail.com>
2025-11-28 02:27:49 +0200

    added read file method to the Main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import java.nio.file.*;
+
 public class Main {
+    public static int[][] grid;
+
+
+    public static void readFromFile(String fileName) {
+        grid = new int[9][9];
+        Path p = Paths.get(fileName);
+        if (!Files.exists(p))
+            return;
+        try (BufferedReader br = Files.newBufferedReader(p)) {
+            String line;
+            int i=0;
+            while ((line = br.readLine()) != null) {
+                String[] split = line.split(",");
+                for(int j=0;j<9;j++){
+                    grid[i][j] = Integer.parseInt(split[j]);
+                }
+                i++;
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading " + fileName + ": " + e.getMessage());
+        }
+        return;
+    }
+
+
+
+
     public static void main(String[] args) {
-        // bla bla bla
+        String file = "src/Data2.csv";
+        readFromFile(file);
+
+        Mode0 mode0 = new Mode0(grid);
+        mode0.run();
+
     }
 }
